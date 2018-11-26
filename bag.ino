61484fd952416bef35d3589c393041e4eb10f81c
Nonthakorn Taboonpong <catsfinetoo@gmail.com>
2018-11-26 21:24:42 +0700

    update

--- bag.ino
+++ bag.ino
@@ -1,7 +1,17 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+#define USE_SERIAL Serial
 bool alarm = false;
 bool letAlarm = true;
 bool stopAlarm = false;
 const double THRESHOLD;
+int speakerPin, freq;
 
 void sendAlarmToServer(){
   // TODO
@@ -13,10 +23,22 @@ void sendAckToServer(){
 
 void getDataFromServer(){
   // TODO
+  HTTPClient http;
+  http.begin("ServerName");
+  int httpCode = http.GET();
+  if(httpCode > 0){
+    String payLoad = http.getString();
+    StaticJsonBuffer<256> jsonBuffer;
+    JsonObject& object = jsonBuffer.parseObject(payload);
+    //TODO WITH OBJECT
+  }
+  
 }
 
 void setup() {
   // put wyour setup code here, to run once:
+  pinMode(speakerPin ,OUTPUT);
+  digitalWrite(speakerPin, LOW);
 
 }
 
@@ -32,9 +54,11 @@ void loop() {
       sendAlarmToServer();
       while(!stopAlarm){
         //play sound
+        tone(speakerPin, freq);
         getDataFromServer();
         
       }
+      noTone(speakerPin);
       senAckToServer();
       alarm = false;
     }
